c59148854e7f707ea73a559d3acfdf2a39bc90a5
Gabryel-777 <gabryeldasilvalemos@gmail.com>
2025-03-13 23:31:28 -0300

    programa continua a ser executado apos qualquel tecla ser clicada

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Os Três Porquinhos");
+Console.ResetColor();
+Console.WriteLine (@"Três porquinhos saem de casa e constroem suas próprias moradias.
+ O primeiro faz uma casa de palha, o segundo de madeira e o terceiro de tijolos. 
+O lobo mau aparece, e sabe o que acontece após isso?");
+Console.WriteLine ("Pressione uma tecla para continuar...");
+Console.ReadKey ();
+Console.WriteLine("Ele mata geral");
+Console.WriteLine("END");
